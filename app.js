bcf3394d5d6c7c8bf6dbe3eb7fb358cc48eadd2d
SandraSuresh-0701 <sandrasuresh0701@gmail.com>
2024-05-16 19:12:42 +0530

    add

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//importing
+// Importing
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -6,16 +6,21 @@ const projectRoutes = require('./routes/projectRoutes');
 const referenceMaterialRouter = require('./routes/materialRoutes');
 const adminAuthRoutes = require('./routes/adminAuthRoutes');
 const authRoutes = require('./routes/authRoutes'); 
-require('dotenv').config()
-const PORT = process.env.PORT;
-require('./DB/connection')
+require('dotenv').config();
 const mentorRoute = require('./routes/mentorRoutes');
 const studentRoute = require('./routes/studentRoute');
 
-//initialisation
+// Initialization
 const app = express();
+const PORT = process.env.PORT || 3000;
+require('./DB/connection');
+
+// Middleware
 app.use(morgan('dev'));
 app.use(cors());
+app.use(express.json()); // Ensure you can handle JSON requests
+
+// Define Routes
 app.use('/api', mentorRoute);
 app.use('/api/project', projectRoutes);
 app.use('/api/reference-material', referenceMaterialRouter);
@@ -23,7 +28,12 @@ app.use('/api/admin', adminAuthRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/student', studentRoute);
 
-//listen
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the MERN Backend!');
+});
+
+// Listen
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
